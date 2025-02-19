23cba78f969b53123c0a3fddf61fc3b925c6ea2e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-19 21:56:15 +0100

    fix: Use Waystone isOwner instead of comparing ids manually

--- common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
+++ common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
@@ -2,10 +2,8 @@ package net.blay09.mods.waystones.compat;
 
 import net.blay09.mods.unbreakables.api.UnbreakablesAPI;
 import net.blay09.mods.unbreakables.rules.parameters.NoParameter;
-import net.blay09.mods.waystones.api.IWaystone;
+import net.blay09.mods.waystones.api.WaystoneVisibility;
 import net.blay09.mods.waystones.api.WaystonesAPI;
-
-import java.util.Objects;
 
 public class UnbreakablesIntegration {
     public UnbreakablesIntegration() {
@@ -13,7 +11,7 @@ public class UnbreakablesIntegration {
             final var server = context.getPlayer().getServer();
             if (server != null) {
                 final var waystone = WaystonesAPI.getWaystoneAt(server, context.getBlockGetter(), context.getPos());
-                return waystone.map(it -> Objects.equals(context.getPlayer().getGameProfile().getId(), it.getOwnerUid()))
+                return waystone.map(it -> it.isOwner(context.getPlayer()))
                         .orElse(true); // waystone not found -> allow breaking by default
             }
             return true; // We default to true since this can only be verified on the server.
@@ -23,7 +21,7 @@ public class UnbreakablesIntegration {
             final var server = context.getPlayer().getServer();
             if (server != null) {
                 final var waystone = WaystonesAPI.getWaystoneAt(server, context.getBlockGetter(), context.getPos());
-                return waystone.map(IWaystone::isGlobal)
+                return waystone.map(it -> it.getVisibility() == WaystoneVisibility.GLOBAL)
                         .orElse(true); // waystone not found -> allow breaking by default
             }
             return true; // We default to true since this can only be verified on the server.
