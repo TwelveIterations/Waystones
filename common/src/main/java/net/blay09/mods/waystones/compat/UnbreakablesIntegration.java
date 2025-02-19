67e78d132eef4956e502d8ecae39c00135490ac7
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-19 21:56:15 +0100

    fix: Use Waystone isOwner instead of comparing ids manually

--- common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
+++ common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
@@ -5,15 +5,13 @@ import net.blay09.mods.unbreakables.rules.parameters.NoParameter;
 import net.blay09.mods.waystones.api.IWaystone;
 import net.blay09.mods.waystones.api.WaystonesAPI;
 
-import java.util.Objects;
-
 public class UnbreakablesIntegration {
     public UnbreakablesIntegration() {
         UnbreakablesAPI.registerCondition("is_waystone_owner", NoParameter.class, (context, params) -> {
             final var server = context.getPlayer().getServer();
             if (server != null) {
                 final var waystone = WaystonesAPI.getWaystoneAt(server, context.getBlockGetter(), context.getPos());
-                return waystone.map(it -> Objects.equals(context.getPlayer().getGameProfile().getId(), it.getOwnerUid()))
+                return waystone.map(it -> it.isOwner(context.getPlayer()))
                         .orElse(true); // waystone not found -> allow breaking by default
             }
             return true; // We default to true since this can only be verified on the server.
