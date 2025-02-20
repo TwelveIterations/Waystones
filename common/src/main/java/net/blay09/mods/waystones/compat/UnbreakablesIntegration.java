5d9a1036213c6885b6b3e88c6209884b4d9f7a18
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-20 19:44:23 +0100

    fix: Use server-side conditions for Unbreakables integration

--- common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
+++ common/src/main/java/net/blay09/mods/waystones/compat/UnbreakablesIntegration.java
@@ -7,24 +7,16 @@ import net.blay09.mods.waystones.api.WaystonesAPI;
 
 public class UnbreakablesIntegration {
     public UnbreakablesIntegration() {
-        UnbreakablesAPI.registerCondition("is_waystone_owner", NoParameter.class, (context, params) -> {
-            final var server = context.getPlayer().getServer();
-            if (server != null) {
-                final var waystone = WaystonesAPI.getWaystoneAt(server, context.getBlockGetter(), context.getPos());
-                return waystone.map(it -> it.isOwner(context.getPlayer()))
-                        .orElse(true); // waystone not found -> allow breaking by default
-            }
-            return true; // We default to true since this can only be verified on the server.
-        });
+        UnbreakablesAPI.registerCondition("is_waystone_owner", NoParameter.class, (context, params) -> context.viaServer((level) -> {
+            final var waystone = WaystonesAPI.getWaystoneAt(level.getServer(), context.getBlockGetter(), context.getPos());
+            return waystone.map(it -> it.isOwner(context.getPlayer()))
+                    .orElse(true); // waystone not found -> allow breaking by default
+        }));
 
-        UnbreakablesAPI.registerCondition("is_waystone_global", NoParameter.class, (context, params) -> {
-            final var server = context.getPlayer().getServer();
-            if (server != null) {
-                final var waystone = WaystonesAPI.getWaystoneAt(server, context.getBlockGetter(), context.getPos());
-                return waystone.map(IWaystone::isGlobal)
-                        .orElse(true); // waystone not found -> allow breaking by default
-            }
-            return true; // We default to true since this can only be verified on the server.
-        });
+        UnbreakablesAPI.registerCondition("is_waystone_global", NoParameter.class, (context, params) -> context.viaServer((level) -> {
+            final var waystone = WaystonesAPI.getWaystoneAt(level.getServer(), context.getBlockGetter(), context.getPos());
+            return waystone.map(IWaystone::isGlobal)
+                    .orElse(true); // waystone not found -> allow breaking by default
+        }));
     }
 }
