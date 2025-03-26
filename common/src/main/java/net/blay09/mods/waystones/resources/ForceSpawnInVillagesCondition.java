0af573ac34df44d5ad79d4cceefc9e8c51d41ce6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-26 13:49:16 +0100

    fix: Fix compile error from cherry-pick

--- common/src/main/java/net/blay09/mods/waystones/resources/ForceSpawnInVillagesCondition.java
+++ common/src/main/java/net/blay09/mods/waystones/resources/ForceSpawnInVillagesCondition.java
@@ -4,13 +4,12 @@ import com.mojang.serialization.MapCodec;
 import net.blay09.mods.balm.api.resources.BalmResourceCondition;
 import net.blay09.mods.balm.api.resources.ResourceConditionContext;
 import net.blay09.mods.waystones.config.WaystonesConfig;
-import net.blay09.mods.waystones.config.WaystonesConfigData;
 
 public class ForceSpawnInVillagesCondition implements BalmResourceCondition {
     public static final MapCodec<ForceSpawnInVillagesCondition> CODEC = MapCodec.unit(ForceSpawnInVillagesCondition::new);
 
     @Override
     public boolean test(ResourceConditionContext resourceConditionContext) {
-        return WaystonesConfig.getActive().worldGen.spawnInVillages == WaystonesConfigData.VillageWaystoneGeneration.FREQUENT;
+        return WaystonesConfig.getActive().worldGen.spawnInVillages == WaystonesConfig.VillageWaystoneGeneration.FREQUENT;
     }
 }
