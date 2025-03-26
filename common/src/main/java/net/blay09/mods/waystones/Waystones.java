45433743ee009e4f62cfc8953db87c4eb3bc90fe
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-26 16:42:02 +0100

    fix: Fix compile error from cherry-pick

--- common/src/main/java/net/blay09/mods/waystones/Waystones.java
+++ common/src/main/java/net/blay09/mods/waystones/Waystones.java
@@ -13,6 +13,7 @@ import net.blay09.mods.waystones.handler.ModEventHandlers;
 import net.blay09.mods.waystones.item.ModItems;
 import net.blay09.mods.waystones.menu.ModMenus;
 import net.blay09.mods.waystones.network.ModNetworking;
+import net.blay09.mods.waystones.resources.ForceSpawnInVillagesCondition;
 import net.blay09.mods.waystones.stats.ModStats;
 import net.blay09.mods.waystones.worldgen.ModWorldGen;
 import net.minecraft.resources.ResourceLocation;
