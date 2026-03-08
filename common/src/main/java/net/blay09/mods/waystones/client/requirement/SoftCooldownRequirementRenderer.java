634908f941e4a63d24d3cbdc03ca86357dad28f1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-08 21:48:14 +0100

    feat(26.1): Re-add real time display of time left on cooldowns

--- common/src/main/java/net/blay09/mods/waystones/client/requirement/SoftCooldownRequirementRenderer.java
+++ common/src/main/java/net/blay09/mods/waystones/client/requirement/SoftCooldownRequirementRenderer.java
@@ -6,14 +6,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class SoftCooldownRequirementRenderer implements RequirementRenderer<CooldownInformation> {
     @Override
     public void renderWidget(Player player, CooldownInformation requirement, GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks, int x, int y) {
-        final var timeLeftStr = formatTimeLeft(player, requirement);
+        final var timeLeftStr = CooldownRenderUtils.formatTimeLeft(requirement);
         if (timeLeftStr == null) return;
         final var font = Minecraft.getInstance().font;
         guiGraphics.drawString(font, timeLeftStr, x + 1, y + font.lineHeight / 2, 0xFFFFAAAA);
@@ -21,27 +20,15 @@ public class SoftCooldownRequirementRenderer implements RequirementRenderer<Cool
 
     @Override
     public int getWidth(Player player, CooldownInformation requirement) {
-        final var timeLeftStr = formatTimeLeft(player, requirement);
+        final var timeLeftStr = CooldownRenderUtils.formatTimeLeft(requirement);
         if (timeLeftStr == null) return 0;
         final var font = Minecraft.getInstance().font;
         return font.width(timeLeftStr) + 1;
     }
 
-    private static @Nullable String formatTimeLeft(Player player, CooldownInformation requirement) {
-        final long millisLeft = (long) (requirement.remainingTicks() / 20f * 1000);
-        if (millisLeft <= 0) {
-            return null;
-        }
-
-        var secondsLeft = millisLeft / 1000;
-        var minutesLeft = secondsLeft / 60;
-        secondsLeft %= 60;
-        return String.format("%02d:%02d", minutesLeft, secondsLeft);
-    }
-
     @Override
     public void appendHoverText(Player player, CooldownInformation requirement, List<Component> tooltip) {
-        final long millisLeft = (long) (requirement.remainingTicks() / 10f * 1000);
+        final long millisLeft = CooldownRenderUtils.getMillisLeft(requirement);
         if (millisLeft > 0) {
             tooltip.add(Component.translatable("tooltip.waystones.cooldown_left", millisLeft / 1000).withStyle(ChatFormatting.GOLD));
         }
