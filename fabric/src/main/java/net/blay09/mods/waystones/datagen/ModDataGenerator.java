a3e7da16e1caab8949a5a55c058fd47fbd5e305e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-14 09:42:16 +0100

    feat: Export statically expected i18n keys during datagen

--- fabric/src/main/java/net/blay09/mods/waystones/datagen/ModDataGenerator.java
+++ fabric/src/main/java/net/blay09/mods/waystones/datagen/ModDataGenerator.java
@@ -1,7 +1,11 @@
 package net.blay09.mods.waystones.datagen;
 
+import net.blay09.mods.balm.client.platform.util.I18nExport;
+import net.blay09.mods.waystones.Waystones;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+import java.io.File;
 
 public class ModDataGenerator implements DataGeneratorEntrypoint {
     @Override
@@ -13,5 +17,7 @@ public class ModDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ModBlockLootTableProvider::new);
         pack.addProvider(ModModelProvider::new);
         pack.addProvider(ModRecipeProvider::new);
+
+        I18nExport.writeStaticI18nKeys(Waystones.MOD_ID, new File("i18n.export.json"));
     }
 }
