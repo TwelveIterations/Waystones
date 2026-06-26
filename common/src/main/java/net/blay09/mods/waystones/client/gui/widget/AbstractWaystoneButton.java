9924f1c05a569507032b22bfbf720bddfad44bbf
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-26 17:56:58 +0200

    feat: Use group color for waystone button

--- common/src/main/java/net/blay09/mods/waystones/client/gui/widget/AbstractWaystoneButton.java
+++ common/src/main/java/net/blay09/mods/waystones/client/gui/widget/AbstractWaystoneButton.java
@@ -1,17 +1,17 @@
 package net.blay09.mods.waystones.client.gui.widget;
 
 import net.blay09.mods.waystones.api.Waystone;
-import net.blay09.mods.waystones.api.WaystoneTypes;
-import net.blay09.mods.waystones.api.WaystoneVisibility;
-import net.minecraft.ChatFormatting;
+import net.blay09.mods.waystones.api.WaystoneGroup;
+import net.blay09.mods.waystones.core.PlayerWaystoneManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+
+import java.util.Optional;
 
 import static net.blay09.mods.waystones.Waystones.id;
 
@@ -31,15 +31,25 @@ public abstract class AbstractWaystoneButton extends Button {
         if (effectiveName.getString().isEmpty()) {
             effectiveName = Component.translatable("gui.waystones.waystone_selection.unnamed_waystone");
         }
-        if (waystone.getVisibility() == WaystoneVisibility.GLOBAL && waystone.getWaystoneType().equals(WaystoneTypes.WAYSTONE)) {
-            effectiveName.withStyle(ChatFormatting.YELLOW);
+        final var firstGroup = getFirstGroup(waystone);
+        if (firstGroup.isPresent()) {
+            effectiveName.withColor(firstGroup.get().color() & 0x00FFFFFF);
         }
         return effectiveName;
     }
 
-    @Override
-    public void renderString(GuiGraphics guiGraphics, Font font, int color) {
-        renderScrollingString(guiGraphics, font, 2 + getDimensionOverlayWidth(), color);
+    private static Optional<WaystoneGroup> getFirstGroup(Waystone waystone) {
+        final var player = Minecraft.getInstance().player;
+        if (player == null) {
+            return Optional.empty();
+        }
+
+        final var waystoneGroups = waystone.getWaystoneGroups();
+        return PlayerWaystoneManager.getPlayerWaystoneData(player.level())
+                .getWaystoneGroupRegistry(player)
+                .stream()
+                .filter(group -> waystoneGroups.contains(group.identifier()))
+                .findFirst();
     }
 
     protected int renderDimensionOverlay(GuiGraphics guiGraphics) {
