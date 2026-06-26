3bbd0e76782eca939e8fdf98ec1e1b72d081c464
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-26 15:59:48 +0200

    feat: Use group color for group buttons

--- common/src/main/java/net/blay09/mods/waystones/client/gui/widget/ManageWaystoneGroupButton.java
+++ common/src/main/java/net/blay09/mods/waystones/client/gui/widget/ManageWaystoneGroupButton.java
@@ -4,6 +4,7 @@ import net.blay09.mods.waystones.api.WaystoneGroup;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
 
 public class ManageWaystoneGroupButton extends Button {
 
@@ -12,10 +13,18 @@ public class ManageWaystoneGroupButton extends Button {
     private final WaystoneGroup group;
 
     public ManageWaystoneGroupButton(int width, WaystoneGroup group) {
-        super(0, 0, width, 20, group.name(), button -> {
+        super(0, 0, width, 20, coloredGroupName(group), button -> {
         }, Button.DEFAULT_NARRATION);
         this.group = group;
         active = false;
+    }
+
+    private static Component coloredGroupName(WaystoneGroup group) {
+        return group.name().copy().withColor(group.color() & 0x00FFFFFF);
+    }
+
+    private static int groupTextColor(WaystoneGroup group) {
+        return 0xFF000000 | (group.color() & 0x00FFFFFF);
     }
 
     @Override
@@ -30,6 +39,6 @@ public class ManageWaystoneGroupButton extends Button {
         final int labelLeft = getX() + TEXT_MARGIN + 18;
         final int maxWidth = getX() + getWidth() - TEXT_MARGIN - labelLeft;
         final var label = font.plainSubstrByWidth(group.name().getString(), maxWidth);
-        graphics.drawString(font, label, labelLeft, getY() + 6, 0xFFFFFFFF, true);
+        graphics.drawString(font, label, labelLeft, getY() + 6, groupTextColor(group), true);
     }
 }
