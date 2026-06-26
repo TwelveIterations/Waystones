03c53e018e8753080f7030ece1214926bd713c57
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-26 13:18:04 +0200

    feat: Add group filters to waystone selection screen

---
+++ common/src/main/java/net/blay09/mods/waystones/client/gui/widget/WaystoneGroupFilterButton.java
@@ -0,0 +1,31 @@
+package net.blay09.mods.waystones.client.gui.widget;
+
+import net.blay09.mods.waystones.api.WaystoneGroup;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.network.chat.Component;
+
+import java.util.function.BooleanSupplier;
+
+public class WaystoneGroupFilterButton extends Button {
+
+    private final WaystoneGroup group;
+    private final BooleanSupplier selected;
+
+    public WaystoneGroupFilterButton(int x, int y, WaystoneGroup group, BooleanSupplier selected, OnPress pressable) {
+        super(x, y, 20, 20, Component.empty(), pressable, Button.DEFAULT_NARRATION);
+        this.group = group;
+        this.selected = selected;
+        setTooltip(Tooltip.create(group.name()));
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        super.renderWidget(graphics, mouseX, mouseY, partialTicks);
+        graphics.blitSprite(group.icon(), getX() + 2, getY() + 2, 16, 16);
+        if (selected.getAsBoolean()) {
+            graphics.renderOutline(getX() + 1, getY() + 1, getWidth() - 2, getHeight() - 2, 0xFFFFFFFF);
+        }
+    }
+}
