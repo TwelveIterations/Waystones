0b9effd8902a6ea3a1c5b622f3fdb6e4556128e4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-30 07:52:50 +0200

    feat: Change waystone runes to use a block model for rendering #1143

--- common/src/main/java/net/blay09/mods/waystones/client/render/WaystoneRenderer.java
+++ common/src/main/java/net/blay09/mods/waystones/client/render/WaystoneRenderer.java
@@ -11,27 +11,24 @@ import net.blay09.mods.waystones.config.WaystonesConfig;
 import net.blay09.mods.waystones.core.PlayerWaystoneManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.BlockModelRenderState;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
-import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.resources.model.sprite.SpriteGetter;
-import net.minecraft.client.resources.model.sprite.SpriteId;
 import net.minecraft.core.Direction;
-import net.minecraft.resources.Identifier;
 import net.minecraft.util.LightCoordsUtil;
-import net.minecraft.util.Unit;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.Nullable;
+import org.joml.Matrix4f;
 
 public class WaystoneRenderer implements BlockEntityRenderer<WaystoneBlockEntity, WaystoneRenderer.WaystoneRenderState> {
 
     public static class WaystoneRenderState extends BlockEntityRenderState {
+        public final BlockModelRenderState runes = new BlockModelRenderState();
         public boolean skip;
         public Direction facing = Direction.NORTH;
         public boolean glow;
@@ -39,15 +36,7 @@ public class WaystoneRenderer implements BlockEntityRenderer<WaystoneBlockEntity
         public int runeColor;
     }
 
-    private static final SpriteId MATERIAL = new SpriteId(TextureAtlas.LOCATION_BLOCKS,
-            Identifier.withDefaultNamespace("waystone_overlays/waystone_active"));
-
-    private final SpriteGetter materials;
-    private final SharestoneModel model;
-
     public WaystoneRenderer(BlockEntityRendererProvider.Context context) {
-        materials = context.sprites();
-        model = new SharestoneModel(context.bakeLayer(ModRenderers.waystoneModel));
     }
 
     @Override
@@ -58,6 +47,11 @@ public class WaystoneRenderer implements BlockEntityRenderer<WaystoneBlockEntity
     @Override
     public void extractRenderState(WaystoneBlockEntity blockEntity, WaystoneRenderState renderState, float delta, Vec3 vec, ModelFeatureRenderer.@Nullable CrumblingOverlay crumblingOverlay) {
         BlockEntityRenderer.super.extractRenderState(blockEntity, renderState, delta, vec, crumblingOverlay);
+        renderState.skip = false;
+        renderState.glow = false;
+        renderState.showRunes = false;
+        renderState.runes.clear();
+
         final var blockState = blockEntity.getBlockState();
         if (blockState.getValue(SharestoneBlock.HALF) != DoubleBlockHalf.LOWER) {
             renderState.skip = true;
@@ -75,6 +69,14 @@ public class WaystoneRenderer implements BlockEntityRenderer<WaystoneBlockEntity
         } else {
             renderState.glow = !WaystonesConfig.getActive().client.disableTextGlow;
         }
+
+        if (renderState.showRunes) {
+            final var model = ModRenderers.waystoneRunesModel.asBlockStateModel();
+            final var modelParts = renderState.runes.setupModel(new Matrix4f(), false);
+            final var random = renderState.runes.scratchRandomSource(blockState.getSeed(blockEntity.getBlockPos()));
+            model.collectParts(random, modelParts);
+            renderState.runes.tintLayers().add(renderState.runeColor);
+        }
     }
 
     @Override
@@ -84,14 +86,11 @@ public class WaystoneRenderer implements BlockEntityRenderer<WaystoneBlockEntity
         }
 
         poseStack.pushPose();
-        poseStack.translate(0.5f, 0f, 0.5f);
-        poseStack.mulPose(Axis.YP.rotationDegrees(renderState.facing.toYRot()));
-        poseStack.mulPose(Axis.XN.rotationDegrees(180f));
-        poseStack.scale(0.5f, 0.5f, 0.5f);
         if (renderState.showRunes) {
-            poseStack.scale(1.05f, 1.05f, 1.05f);
-            final var sprite = materials.get(MATERIAL);
-            submitNodeCollector.submitModel(model, Unit.INSTANCE, poseStack, MATERIAL.renderType(RenderTypes::entityCutout), renderState.glow ? LightCoordsUtil.FULL_BRIGHT : renderState.lightCoords, OverlayTexture.NO_OVERLAY, renderState.runeColor, sprite, 0, renderState.breakProgress);
+            poseStack.translate(0.5f, 0f, 0.5f);
+            poseStack.mulPose(Axis.YP.rotationDegrees(renderState.facing.toYRot()));
+            poseStack.translate(-0.5f, 0f, -0.5f);
+            renderState.runes.submit(poseStack, submitNodeCollector, renderState.glow ? LightCoordsUtil.FULL_BRIGHT : renderState.lightCoords, OverlayTexture.NO_OVERLAY, 0);
         }
         poseStack.popPose();
     }
