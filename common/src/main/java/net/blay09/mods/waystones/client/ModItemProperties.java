0064808240b80ad2db45b02e3cee845fd98eff3e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-03 13:31:55 +0200

    fix(1.21.1): Move item property registration to client started event, to avoid possible race conditions with common/client entrypoints loading simultaneously (if that's even still a thing)

--- common/src/main/java/net/blay09/mods/waystones/client/ModItemProperties.java
+++ common/src/main/java/net/blay09/mods/waystones/client/ModItemProperties.java
@@ -1,13 +1,17 @@
 package net.blay09.mods.waystones.client;
 
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.event.client.ClientStartedEvent;
 import net.blay09.mods.waystones.item.ModItems;
 import net.blay09.mods.waystones.mixin.ItemPropertiesAccessor;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModItemProperties {
     public static void initialize() {
-        ItemPropertiesAccessor.callRegister(ModItems.twinboundFeather, ResourceLocation.withDefaultNamespace("brushing"), (itemStack, level, entity, seed) -> entity != null && entity.getUseItem() == itemStack
-                ? (float) (entity.getUseItemRemainingTicks() % 10) / 10f
-                : 0f);
+        Balm.getEvents().onEvent(ClientStartedEvent.class, event -> {
+            ItemPropertiesAccessor.callRegister(ModItems.twinboundFeather, ResourceLocation.withDefaultNamespace("brushing"), (itemStack, level, entity, seed) -> entity != null && entity.getUseItem() == itemStack
+                    ? (float) (entity.getUseItemRemainingTicks() % 10) / 10f
+                    : 0f);
+        });
     }
 }
