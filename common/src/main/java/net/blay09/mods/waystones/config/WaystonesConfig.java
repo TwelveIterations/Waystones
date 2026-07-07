7110ba8354d9c44e320e011326b96e8177316d4e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-07 22:21:13 +0200

    feat: Add automatic migration of older configured warp requirements if they match the default

--- common/src/main/java/net/blay09/mods/waystones/config/WaystonesConfig.java
+++ common/src/main/java/net/blay09/mods/waystones/config/WaystonesConfig.java
@@ -19,6 +19,14 @@ import java.util.Set;
 @Config(Waystones.MOD_ID)
 public class WaystonesConfig {
 
+    public static final List<String> DEFAULT_WARP_REQUIREMENTS = List.of(
+            "$uses_xp #= any(source(is_waystone), source(is_warp_stone))",
+            "$uses_xp -> $xp_points_cost = $distance * 0.01",
+            "$uses_xp + is_interdimensional -> $xp_points_cost = 27",
+            "$uses_xp -> $xp_points_cost = clamp($xp_points_cost, 0, 27)",
+            "is_warp_stone -> damage_item(80)",
+            "is_inventory_button -> cooldown_cost('inventory_button', '300s')");
+
     public enum TransportMobs implements StringRepresentable {
         ENABLED,
         SAME_DIMENSION,
@@ -94,13 +102,7 @@ public class WaystonesConfig {
         @Synced
         @NestedType(String.class)
         @Comment("List of warp requirements in Shogi format.")
-        public List<String> warpRequirements = List.of(
-                "$uses_xp #= any(source(is_waystone), source(is_warp_stone))",
-                "$uses_xp -> $xp_points_cost = $distance * 0.01",
-                "$uses_xp + is_interdimensional -> $xp_points_cost = 27",
-                "$uses_xp -> $xp_points_cost = clamp($xp_points_cost, 0, 27)",
-                "is_warp_stone -> damage_item(80)",
-                "is_inventory_button -> cooldown_cost('inventory_button', '300s')");
+        public List<String> warpRequirements = DEFAULT_WARP_REQUIREMENTS;
 
         @Synced
         @Comment("Set to \"enabled\" to have nearby pets teleport with you. Set to \"same_dimension\" to have nearby pets teleport with you only if you're not changing dimensions. Set to \"disabled\" to disable.")
