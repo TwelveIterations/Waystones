62cc0c365a82f241312e8bc65e81a57209155300
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-24 16:57:05 +0200

    feat: Add config screen

--- common/src/main/java/net/blay09/mods/waystones/client/WaystonesClient.java
+++ common/src/main/java/net/blay09/mods/waystones/client/WaystonesClient.java
@@ -2,9 +2,11 @@ package net.blay09.mods.waystones.client;
 
 import net.blay09.mods.balm.client.color.block.BalmBlockColorRegistrar;
 import net.blay09.mods.balm.client.gui.screens.inventory.BalmMenuScreenRegistrar;
+import net.blay09.mods.balm.client.platform.config.BalmConfigScreenRegistrar;
 import net.blay09.mods.balm.client.platform.module.BalmClientModule;
 import net.blay09.mods.balm.client.renderer.block.model.BalmBlockStateModelRegistrar;
 import net.blay09.mods.balm.client.renderer.blockentity.BalmBlockEntityRendererRegistrar;
+import net.blay09.mods.waystones.client.config.WaystonesConfigScreenFactory;
 import net.blay09.mods.waystones.Waystones;
 import net.blay09.mods.waystones.client.requirement.RequirementClientRegistry;
 import net.blay09.mods.waystones.compat.Compat;
@@ -43,6 +45,11 @@ public class WaystonesClient implements BalmClientModule {
     @Override
     public void registerMenuScreens(BalmMenuScreenRegistrar screens) {
         ModScreens.initialize(screens);
+    }
+
+    @Override
+    public void registerConfigScreen(BalmConfigScreenRegistrar configScreens) {
+        configScreens.register(WaystonesConfigScreenFactory::create);
     }
 
     @Override
