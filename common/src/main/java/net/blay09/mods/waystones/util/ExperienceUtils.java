45fd600ba9b580c8ebdacbb16af466676200f5d8
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-14 14:01:42 +0200

    fix: Fix incorrect xp cost calculation and integer overflows

--- common/src/main/java/net/blay09/mods/waystones/util/ExperienceUtils.java
+++ common/src/main/java/net/blay09/mods/waystones/util/ExperienceUtils.java
@@ -1,16 +1,12 @@
 package net.blay09.mods.waystones.util;
 
 public class ExperienceUtils {
-    public static int calculateLevelCostFromExperiencePoints(int currentLevel, int xpLoss) {
-        return currentLevel - calculateLevelMinusExperiencePoints(currentLevel, xpLoss);
-    }
-
     public static int calculateMinimumLevelForExperiencePoints(int requiredXp) {
         if (requiredXp <= 0) {
             return 0;
         }
 
-        int cumulativeXp = 0;
+        long cumulativeXp = 0;
         int level = 0;
         while (cumulativeXp < requiredXp) {
             cumulativeXp += getXpNeededForNextLevel(level);
@@ -26,43 +22,30 @@ public class ExperienceUtils {
         }
 
         if (availableXp >= requiredXp) {
-            return Math.max(1, calculateLevelCostFromExperiencePoints(currentLevel, requiredXp));
+            final var remainingXp = availableXp - requiredXp;
+            final var newLevel = calculateLevelFromExperiencePoints(remainingXp);
+            return currentLevel - newLevel;
         }
 
         return calculateMinimumLevelForExperiencePoints(requiredXp);
     }
 
-    private static int calculateLevelMinusExperiencePoints(int currentLevel, int xpLoss) {
-        int currentCumulativeXp = getCumulativeXpNeededForLevel(currentLevel);
-
-        int remainingXp = currentCumulativeXp - xpLoss;
-
-        int newLevel = 0;
-        int newCumulativeXp = 0;
-        for (int level = 0; level <= currentLevel; level++) {
-            newCumulativeXp += getXpNeededForNextLevel(level);
-            if (remainingXp < newCumulativeXp) {
-                newLevel = level;
-                break;
-            }
+    private static int calculateLevelFromExperiencePoints(long experiencePoints) {
+        long cumulativeXp = 0;
+        int level = 0;
+        while (cumulativeXp + getXpNeededForNextLevel(level) <= experiencePoints) {
+            cumulativeXp += getXpNeededForNextLevel(level);
+            level++;
         }
 
-        return newLevel;
+        return level;
     }
 
-    private static int getXpNeededForNextLevel(int level) {
+    private static long getXpNeededForNextLevel(int level) {
         if (level >= 30) {
-            return 112 + (level - 30) * 9;
+            return 112 + (level - 30) * 9L;
         } else {
-            return level >= 15 ? 37 + (level - 15) * 5 : 7 + level * 2;
+            return level >= 15 ? 37 + (level - 15) * 5L : 7 + level * 2L;
         }
-    }
-
-    private static int getCumulativeXpNeededForLevel(int targetLevel) {
-        int currentCumulativeXp = 0;
-        for (int level = 0; level < targetLevel; level++) {
-            currentCumulativeXp += getXpNeededForNextLevel(level);
-        }
-        return currentCumulativeXp;
     }
 }
