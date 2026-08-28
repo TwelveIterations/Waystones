a5449f04d53830471472208a0592072affd3c67f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-28 13:21:36 +0200

    test: Add gametest for warp plate item stack transfer

--- fabric/src/gametest/java/net/blay09/mods/waystones/fabric/gametest/WarpPlateGameTest.java
+++ fabric/src/gametest/java/net/blay09/mods/waystones/fabric/gametest/WarpPlateGameTest.java
@@ -8,10 +8,9 @@ import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.Vec3;
-import org.jspecify.annotations.NonNull;
 
 public class WarpPlateGameTest {
 
@@ -24,6 +23,34 @@ public class WarpPlateGameTest {
         linkWarpPlates(sourcePlate, targetPlate);
 
         final var itemEntity = helper.spawnItem(Items.DIAMOND, Vec3.atCenterOf(sourcePos));
+        itemEntity.setDeltaMovement(Vec3.ZERO);
+
+        helper.assertItemEntityPresent(Items.DIAMOND, sourcePos, 1);
+        helper.assertItemEntityNotPresent(Items.DIAMOND, targetPos, 1);
+
+        helper.runAtTickTime(35, () -> {
+            helper.assertItemEntityNotPresent(Items.DIAMOND, sourcePos, 1);
+            helper.assertEntitiesPresent(EntityType.ITEM, 1);
+            helper.assertEntityInstancePresent(itemEntity, targetPos, 1);
+        });
+        helper.runAtTickTime(75, () -> {
+            helper.assertItemEntityNotPresent(Items.DIAMOND, sourcePos, 1);
+            helper.assertEntitiesPresent(EntityType.ITEM, 1);
+            helper.assertEntityInstancePresent(itemEntity, targetPos, 1);
+            helper.succeed();
+        });
+    }
+
+    @GameTest(maxTicks = 100)
+    public void itemStackTeleportsOnceAndRemainsAtTarget(GameTestHelper helper) {
+        final var sourcePos = new BlockPos(2, 1, 2);
+        final var sourcePlate = setWarpPlate(helper, sourcePos);
+        final var targetPos = new BlockPos(6, 1, 2);
+        final var targetPlate = setWarpPlate(helper, targetPos);
+        linkWarpPlates(sourcePlate, targetPlate);
+
+        final var itemEntity = helper.spawnItem(Items.DIAMOND, Vec3.atCenterOf(sourcePos));
+        itemEntity.setItem(new ItemStack(Items.DIAMOND, 32));
         itemEntity.setDeltaMovement(Vec3.ZERO);
 
         helper.assertItemEntityPresent(Items.DIAMOND, sourcePos, 1);
